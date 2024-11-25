30cd42c1119f0bdccc50f97168308b03b46205c2
Bettenoch <bettenoch254@gmail.com>
2024-11-25 10:28:04 +0300

    FIX: linter errors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> JSONResponse:
     return {"message": "Should be working"}
