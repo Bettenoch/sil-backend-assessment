950bc37dbca1a5129294da02493a3322cf801feb
Bettenoch <bettenoch254@gmail.com>
2024-11-25 10:29:54 +0300

    FIX: linter error on response type

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root() -> JSONResponse:
+async def root() -> dict:
     return {"message": "Should be working"}
