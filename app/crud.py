94078f3f960e61b62bbce2c10bbe1abd7dcf08a1
Bettenoch <bettenoch254@gmail.com>
2024-11-26 10:37:41 +0300

    CREATE: add user crud methpd

---
+++ app/crud.py
@@ -0,0 +1,16 @@
+from sqlmodel import Session
+from app.middleware.user_auth import get_hashed_password
+from app.models import User, UserCreate
+
+
+def create_user(*, create_user: UserCreate, db: Session) -> User:
+    hashed_password = get_hashed_password(create_user.password)
+
+    user_obj = User.model_validate(
+        create_user, update={"hashed_password": hashed_password}
+    )
+
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
