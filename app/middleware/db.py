adefd3056a2d6588b519bffec96b47d2539eeb1f
Bettenoch <bettenoch254@gmail.com>
2024-11-26 10:57:09 +0300

    FIX: type mismatch

--- app/middleware/db.py
+++ app/middleware/db.py
@@ -1,17 +1,16 @@
-from sqlmodel import Session, select, create_engine
+from sqlmodel import Session, create_engine, select
+
 from app import crud
 from app.middleware.preset import settings
-
 from app.models import User, UserCreate
-
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
     top_user = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER_NAME)
-    ).first
+        select(User).where(User.email == settings.FIRST_SUPERUSER_EMAIL)
+    ).first()
 
     if not top_user:
         user_in = UserCreate(
@@ -21,4 +20,4 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
         )
 
-        top_user = crud.create_user(session=session, user_create=user_in)
+        top_user = crud.create_user(db=session, user_create=user_in)
