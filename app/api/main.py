31e3817dcfd70b5ce88ca02b3763c07da6d3a613
Bettenoch <bettenoch254@gmail.com>
2024-11-27 17:34:06 +0300

    CREATE: api router for the new endpoints

---
+++ app/api/main.py
@@ -0,0 +1,12 @@
+
+
+from fastapi import APIRouter
+
+from app.api.routes import health_check, login, users
+
+
+api_router = APIRouter()
+
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(health_check.router, prefix="/health", tags=["health"])
