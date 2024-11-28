4bcc16cea3b66465f44458520754ad298e0ced2e
Bettenoch <bettenoch254@gmail.com>
2024-11-28 13:12:20 +0300

    CREATE: method to get the superuser token headers

--- app/tests/utils/token_gen.py
+++ app/tests/utils/token_gen.py
@@ -1,14 +1,15 @@
-# from app.middleware.preset import settings
+from fastapi.testclient import TestClient
 
-# from fastapi.testclient import TestClient
+from app.middleware.preset import settings
 
-# def get_superuser_headers(client: TestClient) -> dict[str, str]:
-#     login_data = {
-#         "username": settings.FIRST_SUPERUSER_NAME,
-#         "password": settings.FIRST_SUPERUSER_PASSWORD,
-#     }
-#     r = client.post(f"{settings.API_V1_STR}/login/access-token", data=login_data)
-#     tokens = r.json()
-#     a_token = tokens["access_token"]
-#     headers = {"Authorization": f"Bearer {a_token}"}
-#     return headers
+
+def get_superuser_headers(client: TestClient) -> dict[str, str]:
+    login_data = {
+        "username": settings.FIRST_SUPERUSER_EMAIL,
+        "password": settings.FIRST_SUPERUSER_PASSWORD,
+    }
+    req = client.post(f"{settings.API_V1_STR}/login/authenticated_token", data=login_data)
+    tokens = req.json()
+    a_token = tokens["access_token"]
+    headers = {"Authorization": f"Bearer {a_token}"}
+    return headers
