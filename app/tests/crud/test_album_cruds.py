afeb10d6f5c00ce1a31029815dd8eccb6c525c5b
Bettenoch <bettenoch254@gmail.com>
2024-11-30 12:13:40 +0300

    WRITE: unit test for the albums crud methods

---
+++ app/tests/crud/test_album_cruds.py
@@ -0,0 +1,36 @@
+#app/tests/crud/test_album_cruds.py
+
+from faker import Faker
+from sqlmodel import Session
+from app import crud
+from app.models import Album, AlbumCreate, UserCreate
+
+faker = Faker()
+
+sample_email = faker.email()
+sample_username = faker.user_name()
+sample_fullname = faker.name()
+sample_avatar = faker.image_url()
+sample_password = faker.password()
+
+def test_album_create(
+    db: Session
+) -> Album | None:
+    
+    #user create
+    name = sample_fullname
+    username = sample_username
+    email = sample_email
+    password = sample_password
+
+    user_in = UserCreate(email=email, password=password, name=name, username=username)
+    user = crud.create_user(db=db, create_user=user_in)
+    user_id = user.id
+    title = faker.name()
+    description = "First test book"
+    
+    user_in = AlbumCreate(title=title, description=description)
+    album = crud.create_album(db=db, create_album=user_in, owner_id=user_id)
+    
+    assert album.title == title
+    
