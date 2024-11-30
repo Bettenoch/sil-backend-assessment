7e6f895ab07523c4e539a5bbf5010b98f6af825f
Bettenoch <bettenoch254@gmail.com>
2024-11-30 12:14:09 +0300

    WRITE: unit test for the photo crud methods

---
+++ app/tests/crud/test_photo_cruds.py
@@ -0,0 +1,43 @@
+# app/tests/crud/test_photo_cruds.py
+from faker import Faker
+from sqlmodel import Session
+
+from app import crud
+from app.models import AlbumCreate, Photo, PhotoCreate, UserCreate
+
+
+faker = Faker()
+
+sample_email = faker.email()
+sample_username = faker.user_name()
+sample_fullname = faker.name()
+sample_avatar = faker.image_url()
+sample_password = faker.password()
+
+def test_photo_create(
+    *, db: Session
+) -> Photo | None:
+    name = sample_fullname
+    username = sample_username
+    email = sample_email
+    password = sample_password
+
+    user_in = UserCreate(email=email, password=password, name=name, username=username)
+    user = crud.create_user(db=db, create_user=user_in)
+    user_id = user.id
+    title = faker.name()
+    description = "First test book"
+    
+    album_in = AlbumCreate(title=title, description=description)
+    album = crud.create_album(db=db, create_album=album_in, owner_id=user_id)
+    
+    album_id = album.id
+    photo_title = faker.file_name()
+    image_url = faker.image_url()
+    
+    photo_in = PhotoCreate(photo_title=photo_title, image_url=image_url, album_id=album_id)
+    photo = crud.create_photo(db=db, create_photo=photo_in, owner_id=user_id)
+    
+    assert photo.photo_title == photo_title
+    assert photo.image_url == image_url
+    assert photo.owner_id == album.owner_id
