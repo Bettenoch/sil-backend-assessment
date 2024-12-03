aa1793edbf9bcb91306976b2cf95e42b35acf32e
Bettenoch <bettenoch254@gmail.com>
2024-12-03 17:09:19 +0300

    ADD: url to route.

--- app/api/main.py
+++ app/api/main.py
@@ -16,10 +16,10 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(albums.router, prefix="/albums", tags=["albums"])
 api_router.include_router(
-    get_user_albums.router, prefix="/user_albums", tags=["user_albums"]
+    get_user_albums.router, prefix="/users/{user_id}/album", tags=["user_albums"]
 )
 api_router.include_router(photos.router, prefix="/photos", tags=["photos"])
 api_router.include_router(
-    get_album_photos.router, prefix="/album_photos", tags=["album_photos"]
+    get_album_photos.router, prefix="/users/{user_id}/albums/{album_id}/photos", tags=["all_album_photos"]
 )
 api_router.include_router(health_check.router, prefix="/health", tags=["health"])
